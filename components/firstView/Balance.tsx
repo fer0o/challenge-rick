51fe8cd70b95b35d03e5ec36405c5cd8ff9a0971
fer0o <fer0osennin@gmail.com>
2022-06-14 21:34:19 -0500

    add componente icons and balance, solved to view on mobile and desktop

---
+++ components/firstView/Balance.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Balance = () => {
+  return (
+    <div className='border rounded ring-0 ring-gray-50 lg:w-full lg:h-24 w-[300px] h-[86px] lg:block  lg:items-center  p-3 lg:mx-96 mx-9  bg-white '>
+      <span className='font-sans text-base font-semibold uppercase'>
+        Saldo Disponible
+      </span>
+      <h2 className='text-3xl font-sans font-extrabold text-magenta-900'>
+        $1,499,970.00
+      </h2>
+    </div>
+  )
+}
+
+export default Balance
